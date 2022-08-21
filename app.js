36d830888a3111ed9b8cd940803d260378efa108
aamir <aamirjamil25999@gmail.com>
2022-08-21 18:10:29 +0530

    tic tac toe

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("start");
+
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
